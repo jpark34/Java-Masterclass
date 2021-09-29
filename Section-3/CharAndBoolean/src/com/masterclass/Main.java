0465e9a5c5996a0048495a63811f5359d713c6ef
Jarrett Parker <jarrettparker34@gmail.com>
2021-09-29 13:41:10 -0400

    Finished char and boolean video

--- Section-3/CharAndBoolean/src/com/masterclass/Main.java
+++ Section-3/CharAndBoolean/src/com/masterclass/Main.java
@@ -3,6 +3,18 @@ package com.masterclass;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = 'D';
+        char unicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(unicode);
+        char copyrightChar = '\u00A9';
+        System.out.println(copyrightChar);
+
+        boolean trueBool = true;
+        boolean falseBool = false;
+
+        // Example on how a boolean should be named
+        boolean isCustomerOverTwentyOne = true;
     }
 }
