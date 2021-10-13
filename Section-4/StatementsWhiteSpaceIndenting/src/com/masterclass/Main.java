67ffc7d9341887d7aeafb94fc94f2b53cfb69835
Jarrett Parker <jarrettparker34@gmail.com>
2021-10-12 22:52:31 -0400

    Extras Added

---
+++ Section-4/StatementsWhiteSpaceIndenting/src/com/masterclass/Main.java
@@ -0,0 +1,13 @@
+package com.masterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVar = 50;
+        myVar++;
+        int anotherVar = 50; myVar--;
+        System.out.println("This is " +
+                "a valid " +
+                "statement.");
+    }
+}
