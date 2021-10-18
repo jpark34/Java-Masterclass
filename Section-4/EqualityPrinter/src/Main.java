930e636fda49f8f973a30381fd3c4f2b9a7b9e7f
Jarrett Parker <jarrettparker34@gmail.com>
2021-10-17 21:28:32 -0400

    Finished Section 4 and started Section 5

---
+++ Section-4/EqualityPrinter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        EqualityPrinter.printEqual(1, 1, 1);
+        EqualityPrinter.printEqual(1, 1, 2);
+        EqualityPrinter.printEqual(-1, -1, -1);
+        EqualityPrinter.printEqual(1, 2, 3);
+    }
+}
---
+++ Section-4/MinutesToYearsToDaysCalculator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MinutesToYearsToDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsToDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsToDaysCalculator.printYearsAndDays(561600);
+    }
+}
