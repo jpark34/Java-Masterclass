78f7eb8ac4b1be02de5bbcf7f414fcc93e6ee271
Jarrett Parker <jarrettparker34@gmail.com>
2021-10-27 22:02:18 -0400

    finished largest prime factor

---
+++ Section-5/DiagonalStar/src/com/masterclass/Main.java
@@ -0,0 +1,15 @@
+package com.masterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        }
+        
+    }
+}
